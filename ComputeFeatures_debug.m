82f27c3150387e12d5b76bd2bcb93683f68d5e38
rjep <rjep@kth.se>
2013-04-17 16:43:03 +0200

    ComputeFeature.m finished

--- ComputeFeatures_debug.m
+++ ComputeFeatures_debug.m
@@ -1,11 +1,10 @@
 %debug dir('TrainingImages/FACES/')ComputeFeature(ii_ims, ftype)
 %read 100 first images and put all integral images in a list. 
 list = dir('TrainingImages/FACES/');
-ii_ims = [];
-size(ii_ims)
+ii_ims = zeros(19,19,100);
 for i = 3:102
     [im , ii_im]= LoadIm(['TrainingImages/FACES/',list(i).name]);
-    ii_ims = [ii_ims,ii_im];
+    ii_ims(:,:,i-2) = ii_im;
 end
 
 dinfo3 = load('DebugInfo/debuginfo3.mat');
