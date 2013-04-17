d326b7256b2c405cc96e39701de05b0166052434
rjep <rjep@kth.se>
2013-04-17 16:53:24 +0200

    ComputeFeature.m updated3

--- FeatureTypeIII.m
+++ FeatureTypeIII.m
@@ -1,6 +1,6 @@
 function f = FeatureTypeIII(ii_im, x, y, w, h)
 
-f = ComputeBoxSum(ii_im,x+w,y,w,h) - ComputeBoxSum(ii_im,x,y,w,h) - ComputeBoxSum(ii_im,x+2*w,y,w,h);
+f = 2*ComputeBoxSum(ii_im,x+w,y,w,h) - ComputeBoxSum(ii_im,x,y,w*3,h)
 
 end
 
