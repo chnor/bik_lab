3ca8c1ffa9452f44d739b7ef6009e2a30812cd95
tpatel <thibaut.patel@gmail.com>
2013-04-17 17:38:37 +0200

    Some changes in the last debug point

--- ComputeSaveFDataDebug.m
+++ ComputeSaveFDataDebug.m
@@ -1,8 +1,16 @@
+clear all
+
+global fmat
+
+dirname = 'TrainingImages/FACES';
+
 dinfo4 = load('DebugInfo/debuginfo4.mat');
 ni = dinfo4.ni;
-all_ftypes = dinfo4.allftypes;
+all_ftypes = dinfo4.all_ftypes;
 im_sfn = 'FaceData.mat';
-fs_fn = 'FeaturesToMat.mat';
+f_sfn = 'FeaturesToMat.mat';
 rng(dinfo4.jseed);
 LoadSaveImData(dirname, ni, im_sfn);
-ComputeSaveFData(all_ftypes, fs_fn);
+ComputeSaveFData(all_ftypes, f_sfn);
+
+sum(sum(dinfo4.fmat == fmat)) == size(fmat, 1)*size(fmat, 2)
