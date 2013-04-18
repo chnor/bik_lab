a4463e36d8892a8029b6d8ea7a822c6ad8bc62cf
tpatel <thibaut.patel@gmail.com>
2013-04-18 12:17:47 +0200

    Continuing debug ComputeSaveFData

--- ComputeSaveFDataDebug.m
+++ ComputeSaveFDataDebug.m
@@ -16,3 +16,31 @@ H = 19;
 fmat = VecAllFeatures(all_ftypes, W, H);
 
 assert(all(all(fmat == dinfo4.fmat)));
+
+%%
+
+face_fnames = dir(dirname);
+
+files = cell(1, length(face_fnames));
+for i=1:length(face_fnames)
+    name = face_fnames(i).name;
+    files(1, i) = {name};
+end
+
+for i=1:5%length(face_fnames)
+    files(1, i)
+end
+%%
+
+fnums = randi([3,length(files)], 1, ni);
+
+ii_ims = zeros(100, 19*19);
+
+for i=1:ni
+    name = files(1, fnums(i))
+    im_fname = strcat(dirname, '/', name)
+    [im, ii_im] = LoadIm(im_fname);
+    ii_ims(i, :) = ii_im(:);
+end
+
+sum(sum(ii_ims == dinfo4.ii_ims))
