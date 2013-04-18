8e9f0955c93cac3ff95bc18689383c5f55bcfe99
tpatel <thibaut.patel@gmail.com>
2013-04-18 11:31:53 +0200

    Changed LoadSaveImData from cell to matrix

--- LoadSaveImData.m
+++ LoadSaveImData.m
@@ -3,14 +3,15 @@ function LoadSaveImData(dirname, ni, im_sfn)
 face_fnames = dir(dirname);
 fnums = randi([3,length(face_fnames)], 1, ni);
 
-ii_ims = cell(ni, 1);
+ii_ims = zeros(100, 19*19);
 
 for i=1:ni
     im_fname = [dirname, '/', face_fnames(fnums(i)).name];
     [im, ii_im] = LoadIm(im_fname);
-    ii_ims{i} = ii_im;
+    ii_ims(i, :) = ii_im(:);
 end
 
+size(ii_ims)
 save(im_sfn, 'dirname', 'fnums', 'ii_ims');
 
 end
