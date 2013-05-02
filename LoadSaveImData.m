c854943c523e75bdb1c3a9ffeea07fd7ef7723d1
tpatel <thibaut.patel@gmail.com>
2013-05-02 12:03:10 +0200

    Fix for the random issue

--- LoadSaveImData.m
+++ LoadSaveImData.m
@@ -1,7 +1,9 @@
 function LoadSaveImData(dirname, ni, im_sfn)
 
 face_fnames = dir(dirname);
-fnums = randi([3,length(face_fnames)], 1, ni);
+aa = 3:length(face_fnames);
+a = randperm(length(aa));
+fnums = aa(a(1:ni));
 
 ii_ims = zeros(100, 19*19);
 
@@ -11,7 +13,6 @@ for i=1:ni
     ii_ims(i, :) = ii_im(:);
 end
 
-size(ii_ims)
 save(im_sfn, 'dirname', 'fnums', 'ii_ims');
 
 end
