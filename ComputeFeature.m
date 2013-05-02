0429d31ee1ec931d921b6c9717eb893ea177e7e6
rjep <rjep@kth.se>
2013-05-02 10:25:23 +0200

    eps fixed

--- ComputeFeature.m
+++ ComputeFeature.m
@@ -1,6 +1,5 @@
 function fs = ComputeFeature(ii_ims,ftype)
-
-n = length(ii_ims);
+n = size(ii_ims,1);
 fs = zeros(1,n);
 
 type = ftype(1);
@@ -11,13 +10,52 @@ h = ftype(5);
 
 for i=1:n
     if(type ==1)
-        fs(i) = FeatureTypeI(ii_ims(:,:,i),x,y,w,h);
+        fs(i) = FeatureTypeI(reshape(ii_ims(i,:),19,19),x,y,w,h);
     elseif(type == 2)
-        fs(i) = FeatureTypeII(ii_ims(:,:,i),x,y,w,h);
+        fs(i) = FeatureTypeII(reshape(ii_ims(i,:),19,19),x,y,w,h);
     elseif(type == 3)
-        fs(i) = FeatureTypeIII(ii_ims(:,:,i),x,y,w,h);
+        fs(i) = FeatureTypeIII(reshape(ii_ims(i,:),19,19),x,y,w,h);
     elseif(type == 4)
-        fs(i) = FeatureTypeIV(ii_ims(:,:,i),x,y,w,h);
+        fs(i) = FeatureTypeIV(reshape(ii_ims(i,:),19,19),x,y,w,h);
     end
 end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% working but old funtion
+% function fs = ComputeFeature(ii_ims,ftype)
+% 
+% n = length(ii_ims);
+% fs = zeros(1,n);
+% 
+% type = ftype(1);
+% x = ftype(2);
+% y = ftype(3);
+% w = ftype(4);
+% h = ftype(5);
+% for i=1:n
+%     if(type ==1)
+%         fs(i) = FeatureTypeI(ii_ims(i,:),x,y,w,h);
+%     elseif(type == 2)
+%         fs(i) = FeatureTypeII(ii_ims(i,:),x,y,w,h);
+%     elseif(type == 3)
+%         fs(i) = FeatureTypeIII(ii_ims(i,:),x,y,w,h);
+%     elseif(type == 4)
+%         fs(i) = FeatureTypeIV(ii_ims(i,:),x,y,w,h);
+%     end
+% end
+% end
--- FeatureTypeIII.m
+++ FeatureTypeIII.m
@@ -1,6 +1,6 @@
 function f = FeatureTypeIII(ii_im, x, y, w, h)
 
-f = 2*ComputeBoxSum(ii_im,x+w,y,w,h) - ComputeBoxSum(ii_im,x,y,w*3,h)
+f = 2*ComputeBoxSum(ii_im,x+w,y,w,h) - ComputeBoxSum(ii_im,x,y,w*3,h);
 
 end
 
