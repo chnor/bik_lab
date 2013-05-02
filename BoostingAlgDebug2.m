8be4f273217b949608e02ec6435a090922e2516d
Christopher Norman <chnor@csc.kth.se>
2013-05-02 12:53:02 +0200

    Added classifier pic output to debug point 7

--- BoostingAlgDebug2.m
+++ BoostingAlgDebug2.m
@@ -6,3 +6,7 @@ T = dinfo7.T;
 Cparams = BoostingAlg(Fdata, NFdata, FTdata, T);
 sum(abs(dinfo7.alphas - Cparams.alphas) > eps)
 sum(abs(dinfo7.Thetas(:) - Cparams.Thetas(:)) > eps)
+
+cpic = MakeClassifierPic(all_ftypes, Cparams.Thetas(:, 1), ...
+                        Cparams.alphas, Cparams.Thetas(:, 3), 19, 19);
+imagesc(cpic);
