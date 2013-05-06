7584381bcc151db5d9b1e74d72d320ce582131bf
rjep <rjep@kth.se>
2013-05-06 14:20:59 +0200

    checkpoint script 1-2

--- ApplyDetector.m
+++ ApplyDetector.m
@@ -3,6 +3,8 @@ function score = ApplyDetector2(Cparams,ii_im,sig,mu)
     if(nargin<4)
         score = sum(Cparams.alphas.*(Cparams.Thetas(:,3).*(ii_im(:)' * Cparams.fmat(:,Cparams.Thetas(:,1)))' < Cparams.Thetas(:,3).*Cparams.Thetas(:,2)));
     else
-        score = sum(Cparams.alphas.*(Cparams.Thetas(:,3).*(ii_im(:)'*Cparams.fmat(:,Cparams.Thetas(:,1)))'<Cparams.Thetas(:,3).*Cparams.Thetas(:,2)))/sig - (mu*Cparams.all_ftypes(Cparams.Thetas(:,1),4).*Cparams.all_ftypes(Cparams.Thetas(:,1),5)/sig);
+        index = find(Cparams.all_ftypes(:,1)==3);
+        del = (mu*Cparams.all_ftypes(index,4).*Cparams.all_ftypes(index,5)/sig);
+        score = sum(Cparams.alphas.*(Cparams.Thetas(:,3).*(ii_im(:)'*Cparams.fmat(:,Cparams.Thetas(:,1)))'<Cparams.Thetas(:,3).*Cparams.Thetas(:,2)))/sig;
     end
 end
