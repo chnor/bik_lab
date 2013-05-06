831c3a53c8af6e066908a8c03038af2d74b61a5e
rjep <rjep@kth.se>
2013-05-06 12:37:22 +0200

    fixed Applydetect uppgrade

--- ApplyDetectorTest.m
+++ ApplyDetectorTest.m
@@ -1,5 +1,5 @@
 
-function sc = ApplyDetectorTest(Cparams,ii_im)
+function sc = ApplyDetectorTest(Cparams)
     [im, ii_im] = LoadIm('TrainingImages/FACES/face00001.bmp');
     sc= ApplyDetector(Cparams,ii_im);
     assert(abs(sc - 9.1409) < 1e-5);
