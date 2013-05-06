3be092c0b4d60fe1a26d66cf4155c925d82815e8
Christopher Norman <chnor@csc.kth.se>
2013-05-06 12:47:58 +0200

    Initial use new ApplyDetector

--- ScanImageFixedSize.m
+++ ScanImageFixedSize.m
@@ -6,9 +6,12 @@ function dets = ScanImageFixedSize(Cparams, im)
     end
     im = double(im);
     
+%    im = (im - mean(im(:))) / std(im(:));
+    
     dets = [];
     L = 19;
-    theta = 6.5;
+    theta = 8;
+    use_explicit_normalization = true;
     
     ii_im = cumsum(cumsum(im, 1), 2);
     im_square = cumsum(cumsum(im .* im, 1), 2);
@@ -17,17 +20,46 @@ function dets = ScanImageFixedSize(Cparams, im)
     for x = 1:(size(im, 2) - L + 1)
         for y = 1:(size(im, 1) - L + 1)
             im_cur = im(y:(y+L-1), x:(x+L-1));
-%             mu = (ii_im(y+L-1, x+L-1) - ii_im(y, x)) / L^2;
-            mu = mean(im_cur(:));
-%             sigma = sqrt((im_square(y+L-1, x+L-1) - im_square(y, x) - L^2*mu^2) / (L^2 - 1));
-            sigma = std(im_cur(:));
-            im_cur = (im_cur - mu) / sigma;
-            ii_im_cur = cumsum(cumsum(im_cur, 1), 2);
-            response = ApplyDetector(Cparams, ii_im_cur(:));
+            mu = quick_mean(x, y, L, ii_im);
+            sigma = quick_std(x, y, L, mu, im_square);
+            if use_explicit_normalization
+                im_cur = (im_cur - mu) / sigma;
+                ii_im_cur = cumsum(cumsum(im_cur, 1), 2);
+                response = ApplyDetector(Cparams, ii_im_cur(:));
+            else
+                ii_im_cur = cumsum(cumsum(im_cur, 1), 2);
+                response = ApplyDetector(Cparams, ii_im_cur(:), sigma, mu);
+            end
             if response > theta
                 i = i + 1;
                 dets(i, :) = [x, y, x+L-1, y+L-1, response];
             end
         end
     end
+
+function mu = quick_mean(x, y, L, ii_im)
+    mu = ii_im(y+L-1, x+L-1);
+    if x > 1
+        mu = mu - ii_im(y+L-1, x-1);
+    end
+    if y > 1
+        mu = mu - ii_im(y-1, x+L-1);
+    end
+    if x > 1 && y > 1
+        mu = mu + ii_im(y-1, x-1);
+    end
+    mu = mu / (L^2);
+    
+function sigma = quick_std(x, y, L, mu, ii_im)
+    sigma = ii_im(y+L-1, x+L-1);
+    if x > 1
+        sigma = sigma - ii_im(y+L-1, x-1);
+    end
+    if y > 1
+        sigma = sigma - ii_im(y-1, x+L-1);
+    end
+    if x > 1 && y > 1
+        sigma = sigma + ii_im(y-1, x-1);
+    end
+    sigma = sqrt((sigma - L^2 * mu^2) / (L^2 - 1));
     
